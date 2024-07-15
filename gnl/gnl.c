95e045cddb7095598c02f594f97553f46b904e69
mira314 <152292628+mira314@users.noreply.github.com>
2024-07-15 16:42:37 +0300

    test

--- gnl/gnl.c
+++ gnl/gnl.c
@@ -102,6 +102,80 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 	return (str);
 }
 
+char	*ft_fill(int fd, char *readed, char *buffer)
+{
+	int		bytes_read;
+	char	*temp;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(readed);
+			return (0);
+		}
+		else if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = 0;
+		if (!readed)
+			readed = ft_strdup("");
+		temp = readed;
+		readed = ft_strjoin(temp, buffer);
+		free (temp);
+		temp = 0;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (readed);
+}
+char	*ft_set_line(char *line)
+{
+	char	*readed;
+	int		i;
+
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == 0 || line[1] == 0)
+		return (0);
+	readed = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (*readed == 0)
+	{
+		free(readed);
+		readed = 0;
+	}
+	line[i + 1] = 0;
+	return (readed);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*readed;
+	char		*line;
+	char		*buffer;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(buffer);
+		free(readed);
+		readed = 0;
+		buffer = 0;
+		return (0);
+	}
+	if (!buffer)
+		return (0);
+	line = ft_fill(fd, readed, buffer);
+	free(buffer);
+	buffer = 0;
+	if (!line)
+		return (0);
+	readed = ft_set_line(line);
+	return (line);
+}
+
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
